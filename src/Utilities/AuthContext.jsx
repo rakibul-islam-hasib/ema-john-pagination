d971a0603db4c73c01152d0099bd807117c092f7
Hasib Hossain <boraborhasib@gmail.com>
2023-04-16 23:21:02 +0600

    Sign up Updated

--- src/Utilities/AuthContext.jsx
+++ src/Utilities/AuthContext.jsx
@@ -1,11 +1,17 @@
 import React, { createContext } from 'react';
+import { app } from './firebase.init';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 export const userContext = createContext(null);
+const auth = getAuth(app); 
 const AuthContext = ({ children }) => {
 
-    const user = { displayName: 'Hasib Hossain' }
+    const createUser = (email , password) => createUserWithEmailAndPassword(auth , email , password)
+
+
+    const authInfo = {createUser }
     return (
-        <userContext.Provider value={user}>
+        <userContext.Provider value={authInfo}>
             {children}
         </userContext.Provider>
     );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
-
+import React, { useContext, useState } from 'react';
+import { userContext } from '../../Utilities/AuthContext';
+import {MoonLoader} from 'react-spinners'
 const SignUp = () => {
-    
+    const { createUser } = useContext(userContext);
+    const [loading, setLoading] = useState(false);
+    const handelFromSubmit = (e) => {
+        setLoading(true)
+        e.preventDefault()
+        const form = e.target;
+        let email = form.email.value;
+        let password = form.password.value;
+        // console.log(email, password)
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setLoading(false); 
+            })
+            .catch(err => {
+                console.log(err.code)
+                setLoading(false)
+            })
+    }
+
     return (
         <div className='w-[90%] mx-auto'>
-            <div className="border-[1px] border-[#95a0a77a] mt-5 w-[30%] mx-auto signUp-box-shadow  bg-white">
-                <h1 className='text-3xl font-medium text-center my-4'>Sign Up</h1>
-                <form className="w-[80%] mx-auto">
-                    <p className='text-[#2A414F] mb-2'>Email</p>
-                    <input type="email" className='border outline-none w-full py-2 px-3 ' required placeholder='Email' />
-                    <p className='text-[#2A414F] mb-2'>Password</p>
-                    <input type="password" className='border w-full outline-none py-2 px-3 ' required placeholder='Password' />
-                    <p className='text-[#2A414F] mb-2'>Confirm Password</p>
-                    <input type="password" className='border w-full py-2 px-3 outline-none' name="lk" placeholder='Confirm Password' required id="" />
-                    <button className='py-2 w-full bg-[#ff99007f] text-xl font-medium  my-8 rounded-lg '>Sign Up</button>
-                </form>
-            </div>
+            {
+                loading ? <div className="h-screen flex justify-center items-center">
+                        <MoonLoader color="#36d7b7" />
+                </div> :
+                    <div className="border-[1px] border-[#95a0a77a] mt-5 w-[30%] mx-auto signUp-box-shadow  bg-white">
+                        <h1 className='text-3xl font-medium text-center my-4'>Sign Up</h1>
+                        <form onSubmit={handelFromSubmit} className="w-[80%] mx-auto">
+                            <p className='text-[#2A414F] mb-2'>Email</p>
+                            <input type="email" name='email' className='border outline-none w-full py-2 px-3 ' required placeholder='Email' />
+                            <p className='text-[#2A414F] mb-2'>Password</p>
+                            <input type="password" name='password' className='border w-full outline-none py-2 px-3 ' required placeholder='Password' />
+                            <p className='text-[#2A414F] mb-2'>Confirm Password</p>
+                            <input type="password" className='border w-full py-2 px-3 outline-none' name="confPassword" placeholder='Confirm Password' />
+                            <button className='py-2 w-full bg-[#ff99007f] text-xl font-medium  my-8 rounded-lg '>Sign Up</button>
+                        </form>
+                    </div>
+            }
         </div>
     );
 };
